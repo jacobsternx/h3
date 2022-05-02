551b221f4ea1d4963c2a0bc288931709422a85dd
jacobsternx <75816278+jacobsternx@users.noreply.github.com>
2022-05-02 11:45:12 -0700

    h3.sql

--- phpMyAdmin/h3.sql
+++ phpMyAdmin/h3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 02, 2022 at 01:42 AM
+-- Generation Time: May 02, 2022 at 11:44 AM
 -- Server version: 10.6.5-MariaDB
 -- PHP Version: 8.0.18
 
@@ -32,6 +32,15 @@ CREATE TABLE `accounts` (
   `name` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `name`) VALUES
+(1001, 'timbox'),
+(1002, 'southside'),
+(1003, 'galveston');
+
 -- --------------------------------------------------------
 
 --
@@ -40,7 +49,8 @@ CREATE TABLE `accounts` (
 
 CREATE TABLE `events` (
   `id` int(11) NOT NULL,
-  `name` varchar(16) NOT NULL
+  `name` varchar(16) NOT NULL,
+  `team_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -54,7 +64,7 @@ CREATE TABLE `revenue` (
   `bo` decimal(9,2) NOT NULL,
   `pos` decimal(9,2) NOT NULL,
   `associations` decimal(9,2) NOT NULL,
-  `payout_date` int(11) NOT NULL
+  `ticket_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -65,8 +75,19 @@ CREATE TABLE `revenue` (
 
 CREATE TABLE `schools` (
   `id` int(11) NOT NULL,
-  `name` varchar(16) NOT NULL
+  `name` varchar(16) NOT NULL,
+  `account_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `schools`
+--
+
+INSERT INTO `schools` (`id`, `name`, `account_id`) VALUES
+(1001, 'monroe', 1001),
+(1002, 'chandler', 1002),
+(1003, 'saxon', 1002),
+(1004, 'angelitas', 1003);
 
 -- --------------------------------------------------------
 
@@ -76,7 +97,8 @@ CREATE TABLE `schools` (
 
 CREATE TABLE `teams` (
   `id` int(11) NOT NULL,
-  `name` varchar(16) NOT NULL
+  `name` varchar(16) NOT NULL,
+  `school_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -88,7 +110,8 @@ CREATE TABLE `teams` (
 CREATE TABLE `tickets` (
   `id` int(11) NOT NULL,
   `ticket_id` int(11) NOT NULL,
-  `ticket_order` int(11) NOT NULL
+  `ticket_order` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -105,31 +128,36 @@ ALTER TABLE `accounts`
 -- Indexes for table `events`
 --
 ALTER TABLE `events`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `team_id` (`team_id`);
 
 --
 -- Indexes for table `revenue`
 --
 ALTER TABLE `revenue`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ticket_id` (`ticket_id`);
 
 --
 -- Indexes for table `schools`
 --
 ALTER TABLE `schools`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_id` (`account_id`);
 
 --
 -- Indexes for table `teams`
 --
 ALTER TABLE `teams`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `school_id` (`school_id`);
 
 --
 -- Indexes for table `tickets`
 --
 ALTER TABLE `tickets`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `event_id` (`event_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -139,37 +167,71 @@ ALTER TABLE `tickets`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1004;
 
 --
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
 
 --
 -- AUTO_INCREMENT for table `revenue`
 --
 ALTER TABLE `revenue`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
 
 --
 -- AUTO_INCREMENT for table `schools`
 --
 ALTER TABLE `schools`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1006;
 
 --
 -- AUTO_INCREMENT for table `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
 
 --
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`);
+
+--
+-- Constraints for table `revenue`
+--
+ALTER TABLE `revenue`
+  ADD CONSTRAINT `revenue_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`);
+
+--
+-- Constraints for table `schools`
+--
+ALTER TABLE `schools`
+  ADD CONSTRAINT `schools_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
+
+--
+-- Constraints for table `teams`
+--
+ALTER TABLE `teams`
+  ADD CONSTRAINT `teams_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`);
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
