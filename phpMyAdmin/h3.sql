3750cd21b48a48867ddefaf2f908276dead808ef
jacobsternx <75816278+jacobsternx@users.noreply.github.com>
2022-05-01 19:44:10 -0700

    update phpMyAdmin and Snap

--- phpMyAdmin/h3.sql
+++ phpMyAdmin/h3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 01, 2022 at 12:41 PM
+-- Generation Time: May 01, 2022 at 07:38 PM
 -- Server version: 10.6.5-MariaDB
 -- PHP Version: 8.0.18
 
@@ -38,12 +38,12 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`account_id`, `account_name`, `account_csm`) VALUES
-(1, 'taft', 'john'),
-(2, 'timbox', 'linda'),
-(3, 'southside', 'sam'),
-(4, 'duncanville', 'britt'),
-(5, 'casey', 'finn'),
-(6, 'prairie ', 'eric');
+(1001, 'timbox', 'linda'),
+(1002, 'southside', 'sam'),
+(1003, 'duncanville', 'britt'),
+(1004, 'casey', 'finn'),
+(1005, 'prairie ', 'eric'),
+(1006, 'taft', 'john');
 
 -- --------------------------------------------------------
 
@@ -63,14 +63,14 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`event_id`, `event_name`, `event_loc`, `event_policy`) VALUES
-(1, 'temple vs jefferson', 'san diego', 'all sales final'),
-(2, 'xavier vs jefferson', 'tempe', 'not supplied'),
-(3, 'andrew vs jackson', 'mesa', 'no refunds'),
-(4, 'john vs kennedy', 'chandler', 'exchanges allowed'),
-(5, 'lyndon vs johnson', 'chandler', 'not supplied'),
-(6, 'scottsdale vs columbus', 'scottsdale', 'refer to client'),
-(7, 'st joseph vs holy names', 'marysville', 'exchanges allowed.'),
-(8, 'kirkland vs deception point', 'bothel', 'refer to client');
+(1001, 'temple vs jefferson', 'san diego', 'all sales final'),
+(1002, 'xavier vs jefferson', 'tempe', 'not supplied'),
+(1003, 'andrew vs jackson', 'mesa', 'no refunds'),
+(1004, 'john vs kennedy', 'chandler', 'exchanges allowed'),
+(1005, 'lyndon vs johnson', 'chandler', 'not supplied'),
+(1006, 'scottsdale vs columbus', 'scottsdale', 'refer to client'),
+(1007, 'st joseph vs holy names', 'marysville', 'exchanges allowed.'),
+(1008, 'kirkland vs monroe', 'bothel', 'refer to client');
 
 -- --------------------------------------------------------
 
@@ -91,14 +91,14 @@ CREATE TABLE `revenue` (
 --
 
 INSERT INTO `revenue` (`revenue_id`, `revenue_tickets`, `revenue_gate`, `revenue_associations`, `revenue_date`) VALUES
-(1, '3000', '75', '1500', '2022-01-01'),
-(2, '1650', '200', '400', '2022-01-15'),
-(3, '1500', '35', '200', '2022-02-01'),
-(4, '1850', '400', '240', '2022-02-15'),
-(5, '2500', '50', '1500', '2022-03-01'),
-(6, '2250', '150', '220', '2022-03-15'),
-(7, '2250', '25', '50', '2022-04-01'),
-(8, '750', '1250', '50', '2022-04-15');
+(1001, '3000', '75', '1500', '2022-01-01'),
+(1002, '1650', '200', '400', '2022-01-15'),
+(1003, '1500', '35', '200', '2022-02-01'),
+(1004, '1850', '400', '240', '2022-02-15'),
+(1005, '2500', '50', '1500', '2022-03-01'),
+(1006, '2250', '150', '220', '2022-03-15'),
+(1007, '2250', '25', '50', '2022-04-01'),
+(1008, '750', '1250', '50', '2022-04-15');
 
 -- --------------------------------------------------------
 
@@ -117,12 +117,12 @@ CREATE TABLE `schools` (
 --
 
 INSERT INTO `schools` (`school_id`, `school_name`, `school_loc`) VALUES
-(1, 'heather', 'scottsdale'),
-(2, 'jackson', 'phoenix'),
-(3, 'jefferson', 'spokane'),
-(4, 'johnson', 'portland'),
-(5, 'kennedy', 'fremont'),
-(6, 'washington', 'redmond');
+(1001, 'heather', 'scottsdale'),
+(1002, 'jackson', 'phoenix'),
+(1003, 'jefferson', 'spokane'),
+(1004, 'johnson', 'portland'),
+(1005, 'kennedy', 'fremont'),
+(1006, 'washington', 'redmond');
 
 -- --------------------------------------------------------
 
@@ -141,12 +141,12 @@ CREATE TABLE `teams` (
 --
 
 INSERT INTO `teams` (`team_id`, `team_name`, `team_mascot`) VALUES
-(1, 'gymnastics', 'wildcats'),
-(2, 'jv basketball', 'bulldogs'),
-(3, 'varsity baseball', 'tigers'),
-(4, 'varsity basketball', 'eagles'),
-(5, 'varsity swimming', 'bombers'),
-(6, 'varsity wrestling', 'saxons');
+(1001, 'gymnastics', 'wildcats'),
+(1002, 'jv basketball', 'bulldogs'),
+(1003, 'varsity baseball', 'tigers'),
+(1004, 'varsity basketball', 'eagles'),
+(1005, 'varsity swimming', 'bombers'),
+(1006, 'varsity wrestling', 'saxons');
 
 -- --------------------------------------------------------
 
@@ -156,6 +156,7 @@ INSERT INTO `teams` (`team_id`, `team_name`, `team_mascot`) VALUES
 
 CREATE TABLE `tickets` (
   `ticket_id` int(11) NOT NULL,
+  `ticket_id_number` int(11) NOT NULL,
   `ticket_order` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -163,13 +164,13 @@ CREATE TABLE `tickets` (
 -- Dumping data for table `tickets`
 --
 
-INSERT INTO `tickets` (`ticket_id`, `ticket_order`) VALUES
-(10326196, 401),
-(16875324, 624),
-(26978231, 582),
-(47623187, 741),
-(87264183, 821),
-(87298013, 546);
+INSERT INTO `tickets` (`ticket_id`, `ticket_id_number`, `ticket_order`) VALUES
+(1001, 10326196, 401),
+(1002, 16875324, 624),
+(1003, 26978231, 582),
+(1004, 47623187, 741),
+(1005, 87264183, 821),
+(1006, 87298013, 546);
 
 --
 -- Indexes for dumped tables
@@ -219,37 +220,31 @@ ALTER TABLE `tickets`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `account_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `account_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1007;
 
 --
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `event_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `event_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41005;
 
 --
 -- AUTO_INCREMENT for table `revenue`
 --
 ALTER TABLE `revenue`
-  MODIFY `revenue_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `revenue_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1009;
 
 --
 -- AUTO_INCREMENT for table `schools`
 --
 ALTER TABLE `schools`
-  MODIFY `school_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `school_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1007;
 
 --
 -- AUTO_INCREMENT for table `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `tickets`
---
-ALTER TABLE `tickets`
-  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87298014;
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1007;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
